800932fac68e32ee8a468287ab1e1ea0a524dcd6
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-08-12 13:55:29 +0600

    warkspace

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,12 +1,19 @@
+"use client";
 import React from "react";
 import Logo from "./Logo";
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, useAuth, UserButton } from "@clerk/nextjs";
 
 function Header() {
+  const { orgId } = useAuth();
+  console.log(orgId);
+
   return (
     <div className="flex justify-between items-center p-3 shadow-sm">
       <Logo />
-      <OrganizationSwitcher />
+      <OrganizationSwitcher
+        afterCreateOrganizationUrl={"/dashboard"}
+        afterLeaveOrganizationUrl={"/dashboard"}
+      />
       <UserButton />
     </div>
   );
