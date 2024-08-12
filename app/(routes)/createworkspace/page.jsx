878a829849640fcc5fa34222bb5e3ef76b2c8111
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-08-12 14:18:05 +0600

    warkspace

---
+++ app/(routes)/createworkspace/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateWorspace() {
+  return <div>fderew</div>;
+}
+
+export default CreateWorspace;
--- app/(routes)/dashboard/_components/WorkspaceList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceList.jsx
@@ -1,16 +1,51 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
-import React from "react";
+import { AlignLeft, LayoutGrid } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 
 function WorkspaceList() {
   const { user } = useUser();
+
+  const [workspaceList, setWorkspaceList] = useState([]);
+
   return (
     <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
       <div className="flex justify-between">
-        <h2 className="font-bold text-2xl capitalize">Hello, {user?.fullName} </h2>
-        <Button>+</Button>
+        <h2 className="font-bold text-2xl capitalize">
+          Hello, {user?.fullName}{" "}
+        </h2>
+        <Link href={"/createworkspace"}>
+          <Button>+</Button>
+        </Link>
       </div>
+      <div className="flex justify-between mt-10">
+        <h2 className="font-medium text-primary capitalize">Workspace</h2>
+        <div className="flex gap-2">
+          <LayoutGrid />
+          <AlignLeft />
+        </div>
+      </div>
+      {workspaceList?.length === 0 ? (
+        <div className="flex flex-col justify-center items-center">
+          <Image
+            src={"/workspace.png"}
+            width={200}
+            height={200}
+            alt="workspace"
+          />
+          <h2>Create a new Workspace</h2>
+          <Link href={"/createworkspace"}>
+            <Button className="my-3">
+              + New Workspace
+            </Button>
+          </Link>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
