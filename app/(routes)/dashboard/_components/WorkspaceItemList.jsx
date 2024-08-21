a6b9cea8c9775f4a9dff24846dd42936afce0483
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-08-21 23:02:25 +0600

    workspace

--- app/(routes)/dashboard/_components/WorkspaceItemList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceItemList.jsx
@@ -1,12 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
 function WorkspaceItemList({ workspaceList }) {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-6">
       {workspaceList &&
         workspaceList.map((workspace, index) => (
-          <div>
-            <h2>{workspace?.workspaceName}</h2>
+          <div
+            key={index}
+            className="border shadow-xl rounded-xl hover:scale-105 transition-all cursor-pointer"
+          >
+            <Image
+              src={workspace?.coverImage}
+              width={400}
+              height={200}
+              alt="cover"
+              className="h-[150px] object-cover rounded-t-xl"
+            />
+            <div className="p-4 rounded-b-xl">
+              <h2 className="flex gap-2">
+                {workspace?.emoji}
+                {workspace?.workspaceName}
+              </h2>
+            </div>
           </div>
         ))}
     </div>
